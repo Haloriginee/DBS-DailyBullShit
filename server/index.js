7eda9034cab525a44796174b6b21443970169727
Kevin Hang <herezy2006@gmail.com>
2023-02-23 11:31:42 +0100

    mongoDB Setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import { Express } from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./mongodb/connect.js";
+
+dotenv.config();
+
+const app = Express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/", (req, res) => {
+  res.send({ message: 'Obiwan Kenobi in da place!'});
+})
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port http://localhost:8080'));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+startServer();
