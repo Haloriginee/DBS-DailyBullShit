ee4766c6492b4b571e46f50836a1b4eb0bc48471
Kevin Hang <herezy2006@gmail.com>
2023-02-28 13:10:15 +0100

    card visible

--- server/controllers/showcase.controller.js
+++ server/controllers/showcase.controller.js
@@ -13,55 +13,8 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const getAllShowcases = async (req, res) => {
-    const {
-        _end,
-        _order,
-        _start,
-        _sort,
-        title_like = "",
-        showcaseType = "",
-    } = req.query;
 
-    const query = {};
-
-    if (showcaseType !== "") {
-        query.showcaseType = showcaseType;
-    }
-
-    if (title_like) {
-        query.title = { $regex: title_like, $options: "i" };
-    }
-
-    try {
-        const count = await Showcase.countDocuments({ query });
-
-        const showcase = await Showcase.find(query)
-            .limit(_end)
-            .skip(_start)
-            .sort({ [_sort]: _order });
-
-        res.header("x-total-count", count);
-        res.header("Access-Control-Expose-Headers", "x-total-count");
-
-        res.status(200).json(showcase);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
-
-const getShowcaseDetail = async (req, res) => {
-    const { id } = req.params;
-    const showcaseExists = await Showcase.findOne({ _id: id }).populate(
-        "creator",
-    );
-
-    if (showcaseExists) {
-        res.status(200).json(showcaseExists);
-    } else {
-        res.status(404).json({ message: "Showcase not found" });
-    }
-};
+//CREATE
 
 const createShowcase = async (req, res) => {
     try {
@@ -90,55 +43,32 @@ const createShowcase = async (req, res) => {
     }
 };
 
-const updateShowcase = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { title, description, showcaseType, location, day, photo } =
-            req.body;
 
-        const photoUrl = await cloudinary.uploader.upload(photo);
+// READ
 
-        await Showcase.findByIdAndUpdate(
-            { _id: id },
-            {
-                title,
-                description,
-                showcaseType,
-                location,
-                day,
-                photo: photoUrl.url || photo,
-            },
-        );
-
-        res.status(200).json({ message: "Showcase updated successfully" });
-    } catch (error) {
-        res.status(500).json({ message: "fail u suck" });
-    }
+const getAllShowcases = async (req, res) => {
+  try {
+    const showcases = await Showcase.find({}).limit(req.query._end);
+    res.status(200).json(showcases);
+  } catch (error) {
+    res.status(500).json({ message: "Read Failed" })
+  }
 };
 
+const getShowcaseDetail = async (req, res) => {
+
+}
+
+// UPDATE
+
+const updateShowcase = async (req, res) => {
+
+};
+
+// DELETE
+
 const deleteShowcase = async (req, res) => {
-    try {
-        const { id } = req.params;
 
-        const showcaseToDelete = await Showcase.findById({ _id: id }).populate(
-            "creator",
-        );
-
-        if (!showcaseToDelete) throw new Error("Showcase not found");
-
-        const session = await mongoose.startSession();
-        session.startTransaction();
-
-        showcaseToDelete.remove({ session });
-        showcaseToDelete.creator.allShowcases.pull(showcaseToDelete);
-
-        await showcaseToDelete.creator.save({ session });
-        await session.commitTransaction();
-
-        res.status(200).json({ message: "Showcase deleted successfully" });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
 };
 
 export {
